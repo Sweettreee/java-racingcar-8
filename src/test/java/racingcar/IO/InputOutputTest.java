28ea8f24719a83d70845fb5b99af661ee0a60932
Sweettreee <xhrlehgus1234@naver.com>
2025-10-27 00:48:29 +0900

    fix: correct test string to match test input

--- src/test/java/racingcar/IO/InputOutputTest.java
+++ src/test/java/racingcar/IO/InputOutputTest.java
@@ -26,7 +26,7 @@ public class InputOutputTest extends IOTest {
     @Test
     void 입력값_유효_테스트() {
         // given
-        String testString = "car1, car2, car3";
+        String testString = "car1,car2,car3";
         int testNumber = 3;
 
         // when
