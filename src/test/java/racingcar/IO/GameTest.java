422e27961c2de377e716ac389165a3c92a0f957c
Sweettreee <xhrlehgus1234@naver.com>
2025-10-27 15:48:58 +0900

    fix: separate feature and only add test if random number has generated propley(0-9)

--- src/test/java/racingcar/IO/GameTest.java
+++ src/test/java/racingcar/IO/GameTest.java
@@ -2,7 +2,6 @@ package racingcar.IO;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.controller.RacingCarController;
@@ -17,12 +16,20 @@ public class GameTest extends IOTest {
     }
 
     @Test()
-    void 난수생성_및_전진_할지말지_정하기_테스트() {
-        String[] carNames = {"car1", "car2", "car3"};
-        int gameTryNumber = 3;
-        ArrayList<Boolean>[] simulatedMoves = racingCarController.makeWholeMoves(gameTryNumber);
-        assertThat(simulatedMoves)
-                .hasSize(gameTryNumber)
-                .isNotEmpty();
+    void 난수생성() {
+        int simulatedNumber = racingCarController.generateNumber();
+
+        assertThat(simulatedNumber).isBetween(0, 9);
     }
+
+    @Test
+    void 전진_테스트() {
+
+    }
+
+    @Test()
+    void 레이싱카_업데이트_테스트() {
+
+    }
+
 }
